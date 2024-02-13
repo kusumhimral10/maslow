20ce86e88db15b869f3669d269db05f2492a4df1
kusumhimral10 <kusum.himral@primotech.com>
2024-02-13 18:46:46 +0530

    ui changes

---
+++ ui/build/static/js/react-syntax-highlighter_languages_refractor_brainfuck.474be043.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkflowise_ui=self.webpackChunkflowise_ui||[]).push([[5539],{84266:e=>{function a(e){e.languages.brainfuck={pointer:{pattern:/<|>/,alias:"keyword"},increment:{pattern:/\+/,alias:"inserted"},decrement:{pattern:/-/,alias:"deleted"},branching:{pattern:/\[|\]/,alias:"important"},operator:/[.,]/,comment:/\S+/}}e.exports=a,a.displayName="brainfuck",a.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_brainfuck.474be043.chunk.js.map
---
+++ ui/build/static/js/react-syntax-highlighter_languages_refractor_json.dba0e72c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkflowise_ui=self.webpackChunkflowise_ui||[]).push([[3657],{41680:e=>{function n(e){e.languages.json={property:{pattern:/"(?:\\.|[^\\"\r\n])*"(?=\s*:)/,greedy:!0},string:{pattern:/"(?:\\.|[^\\"\r\n])*"(?!\s*:)/,greedy:!0},comment:/\/\/.*|\/\*[\s\S]*?(?:\*\/|$)/,number:/-?\d+\.?\d*(e[+-]?\d+)?/i,punctuation:/[{}[\],]/,operator:/:/,boolean:/\b(?:true|false)\b/,null:{pattern:/\bnull\b/,alias:"keyword"}}}e.exports=n,n.displayName="json",n.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_json.dba0e72c.chunk.js.map
